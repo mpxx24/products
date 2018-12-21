4ccdb9d48fb93b3d1aad47492f9b7b2c0fe37ff7
Mariusz Piątkowski <mpxx24@gmail.com>
2018-12-21 14:11:56 +0100

    Add project files.

---
+++ MOVO.Products.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MOVO.Products.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
